bcf16a512e0d86ac17efc6827cbfa041a7612ba1
stefanweb03 <79223792+stefanweb03@users.noreply.github.com>
2023-01-04 23:10:50 +0200

    Attempt10

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -29,15 +29,15 @@ const Services = (props) => {
                             <h4 className='pt-5 fw-bold align-self-center grey'>With our combined experience, we were able to spot the weak points of roadmaps and blueprints of our clients and give them an honest and sometimes reality checking opinion.</h4>
                             <h4 className='pt-5 fw-bold align-self-center grey'>As we started getting closer, we all realized that we share the same passion and work ethic in making Web3 projects and taking our client’s ideas to a refined product.</h4>
                         </Col>
-                        <Col className='d-flex justify-content-center'>
+                        <Col className='d-flex mt-5 justify-content-center'>
                             <img className="BigImage rounder " src={Image} alt="Italian Trulli" />
                         </Col>
                     </Row>
-                    <Row className='w-75 mt-5 mx-auto text-center d-block '>
-                        <h3 className='pt-5 text-center fw-bold align-self-center'>Our Story</h3>   
-                        <h3 className='pt-5 w-75 fw-bold mx-auto grey'>We are a team of 4 friends who started working in the Web3 space as freelancers more than a year ago.  </h3>
-                        <h3 className='pt-5 w-75 fw-bold mx-auto grey'>With our combined experience, we were able to spot the weak points of roadmaps and blueprints of our clients and give them an honest and sometimes reality checking opinion.</h3>
-                        <h3 className='pt-5 w-75 fw-bold mx-auto grey'>As we started getting closer, we all realized that we share the same passion and work ethic in making Web3 projects and taking our client’s ideas to a refined product.</h3>
+                    <Row className='w-75 mt-5 mx-auto  d-block '>
+                        <h3 className='pt-5 fw-bold align-self-center'>Our Story</h3>   
+                        <h3 className='pt-5  fw-bold  grey'>We are a team of 4 friends who started working in the Web3 space as freelancers more than a year ago.  </h3>
+                        <h3 className='pt-5  fw-bold  grey'>With our combined experience, we were able to spot the weak points of roadmaps and blueprints of our clients and give them an honest and sometimes reality checking opinion.</h3>
+                        <h3 className='pt-5  fw-bold  grey'>As we started getting closer, we all realized that we share the same passion and work ethic in making Web3 projects and taking our client’s ideas to a refined product.</h3>
                     </Row>
                 </div>
             </div>

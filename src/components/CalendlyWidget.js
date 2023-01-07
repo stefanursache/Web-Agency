83c263fa98bdac208ce24149a3554b62f011ee03
stefanweb03 <79223792+stefanweb03@users.noreply.github.com>
2023-01-07 10:51:07 +0200

    try13

--- src/components/CalendlyWidget.js
+++ src/components/CalendlyWidget.js
@@ -4,9 +4,9 @@ const CalendlyWidget = () => {
   return (
     <>
       <div
-        className="calendly-inline-widget"
+        className="calendly-inline-widget noscroll h-auto"
         data-url="https://calendly.com/nflagency/30min"
-        style={{ minWidth: '320px', height: '630px' }}
+        
       />
       <script src="https://assets.calendly.com/assets/external/widget.js" type="text/javascript" async />
     </>
